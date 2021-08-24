d633e63695ec6d351df77bf5f98f3a830b1b972a
Sonam <sonam.malhotra2496@gmail.com>
2021-08-24 23:47:16 +0000

    Final changes for spinner1 and spinner2 made it short

---
+++ spinner2.js
@@ -0,0 +1,35 @@
+// process.stdout.write('hi from spinner1.js... \rhey\n');
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+// // ... fill in the rest yourself ...
+
+
+process.stdout.write('hello from spinner2.js... \rheyyy\n');
+const array =  ['|', '/', '-', '\\','|', '/', '-', '\\','|'];
+let time = 0;
+for (const char in array){
+   setTimeout(() => {
+     process.stdout.write(`\r${array[char]}   `);
+   },time);
+   time+=100;
+}
+setTimeout(() => {
+  console.log('');
+}, 1000);
+
