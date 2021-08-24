2caee9a0f1fb74309db64e7f66d98e07dade10b0
Sonam <sonam.malhotra2496@gmail.com>
2021-08-24 23:30:10 +0000

    Done spinner 1 using \r

---
+++ spinner1.js
@@ -0,0 +1,20 @@
+process.stdout.write('hi from spinner1.js... \rhey\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
